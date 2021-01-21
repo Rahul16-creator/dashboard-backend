ed80163d00adef7e7fa73f32a92131e8ec944153
project-team-ece <rahulkrishnanr2021@srishakthi.ac.in>
2021-01-21 07:10:54 +0530

    INIT

---
+++ app.js
@@ -0,0 +1,40 @@
+const express=require('express')
+const cors=require('cors')
+const bodyParser=require('body-parser')
+const reportRouter=require('./router/report');
+const app=express()
+const port=process.env.PORT || 8000
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST"
+    );
+    next();
+  });
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(reportRouter);
+
+app.listen(port,()=>{
+    console.log(`App is connected to ${port} port`)
+})
+
+
+
+
+
+
